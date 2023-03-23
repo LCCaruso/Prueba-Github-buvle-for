752203f7dbefd2201bac7a31ce649f9edd725508
LCCaruso <lucas.caruso27@gmail.com>
2023-03-23 15:30:34 -0300

    añadimos archivo buclefor.js

---
+++ BucleFOR.js
@@ -0,0 +1,19 @@
+var suma = 0;
+// suma = suma +1;
+// suma = suma +2;
+// suma = suma +3;
+// suma = suma +4;
+
+// console.log(suma)
+
+for (var i=1 ; i<=30 ; i++){
+    suma = suma +i;
+    // if (i == 15) {
+    //     continue;
+    // }
+    // else if (i == 20) {
+    //     continue;
+    // }
+    console.log("Valor del numero: ", i);
+}
+console.log("Suma total: ", suma)
